6072d54c94337f14cc9a574829a26e7682c9bc57
Uchenna Emechebe <emechebe@brbb030.ohsu.edu>
2018-06-20 22:58:19 -0700

    Update on Readme files

---
+++ EverydayStruggleWithoutJoe/Get_data_without_Joe.py
@@ -0,0 +1,95 @@
+# Since I will be getting data from the web, I need  urllib
+#import urllib.request, urllib.parse, urllib.error
+import requests
+import json
+import ssl
+import sqlite3
+import aniso8601
+import datetime
+from datetime import timedelta
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+#We build the database we want to dump the data into
+conn = sqlite3.connect('EveryDayStruggle.sqlite')
+# Create the database filehandle
+cur = conn.cursor()
+
+# This prompts for the url
+url = "https://www.googleapis.com/youtube/v3/search"
+url1='https://www.googleapis.com/youtube/v3/playlists'
+
+api_key ='AIzaSyAjKHMECCdtEqhz6ORJRtbqRVZVJWPCHwE'
+
+lastdate  = "2018-06-20T00:00:00Z"
+startdate ="2017-12-01T00:00:00Z"
+
+for i in xrange(20):
+   print (startdate,lastdate)
+   parameters = {"part": "snippet", "maxResults": 50,"type": "video", "order": "date","pageToken": "","publishedAfter": startdate,"publishedBefore": lastdate,
+   "q": "#EverydayStruggle",'channelId':'UCpFHkjOa7ia6bH5_6cDsDXg',"key": api_key,}
+
+   parameters1 = {"part": "snippet", "maxResults": 50,"type": "video", "order": "date","pageToken": "","q": "EVERYDAY STRUGGLE | Joe Budden & Akademiks",'channelId':'UCpFHkjOa7ia6bH5_6cDsDXg',"key": api_key,}
+
+   parameter = {"part": "snippet", "maxResults": 1,"channelTitle": "Complex News", "order": "date","pageToken": "","publishedAfter": "2017-04-01T00:00:00Z",
+   "q": "#EverydayStruggle",
+   "key": api_key, "type": "video",}
+
+   page = requests.request(method="get", url=url, params=parameters)
+   j_results = json.loads(page.text)
+   if len(j_results['items']) == 0:
+          break 
+   id_video = []
+   title_video = []
+   Date_video = []
+   count = 0
+   for j in j_results['items']:
+	   id_video.append(j['id']['videoId'])
+	   title_video.append(j['snippet']['title'])
+	   Date_video.append(j['snippet']['publishedAt'])
+	   count = count + 1
+	   if count == len(title_video):
+		   lastdate = j['snippet']['publishedAt']
+   date = lastdate
+
+
+   # Now we have the id's , get statistics for every id
+   url_vid = "https://www.googleapis.com/youtube/v3/videos"
+   Statistics = []
+   for k in id_video:
+       parameters_stat = {"part": "statistics","id": k,"key": api_key,}
+       page = requests.request(method="get", url=url_vid, params=parameters_stat)
+       j_results = json.loads(page.text)
+       Statistics.append(j_results)
+   print len(Statistics)
+
+   # I also wanted to get duration of the video
+
+   Duration = []
+   for d in id_video:
+	   parameters_duration = {"part": "contentDetails",'id':d,'key':api_key,}
+	   page = requests.request(method="get", url=url_vid, params=parameters_duration)
+	   d_results = json.loads(page.text)
+	   Duration.append(d_results)
+   # Put together title of podcast and stats of podcasts
+   #print (Duration)
+   myList=[]
+   Data =[]
+   for l in range(len(title_video)):
+	   title = title_video[l]
+	   viewCount = Statistics[l]['items'][0]['statistics']['viewCount']
+	   commentCount = Statistics[l]['items'][0]['statistics']['commentCount']
+	   dislikeCount = Statistics[l]['items'][0]['statistics']['dislikeCount']
+	   favCount = Statistics[l]['items'][0]['statistics']['favoriteCount']
+	   Date = Date_video[l]
+	   Video_length = Duration[l]['items'][0]['contentDetails']['duration']
+	   parsed_Video_length = aniso8601.parse_duration(Video_length)
+
+	   #print parsed_Video_length
+	   cur.execute('''CREATE TABLE IF NOT EXISTS EveryDayStruggle (Date_published DATE,title VARCHAR,viewCount Number,commentCount Number,dislikeCount Number, favCount Number,Video_length Number)''')
+	   cur.execute('''INSERT INTO EveryDayStruggle (Date_published,title,viewCount,commentCount,dislikeCount,favCount,Video_length) VALUES ( ?, ?,?,?,?,?,? )''',(Date,title,viewCount,commentCount,dislikeCount,favCount,Video_length))
+	   conn.commit()
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # EverydayStruggle
+
+## A favorite show of mine is called EverydayStruggle, a show where current hip hop events are discussed. The show initially started with three hosts Nadeska, Joe and Dj Akademiks. Nadeska served as the moderator with Joe and Dj Akademiks as opinion commentators. Joe was the most controversial character of the show and in my opinion , his antics boosted the viewership of the show. However, Joe's tenure lasted from 2017-04-10 to 2017-12-20 as his contract was not renewed for the next season. Joe was replaced by another flamboyant character Dj Star and then later on by Wayno.
+
+## Since I had the opinion that Joe was instrumental to the show's take off, I hypothesized that Joe leaving the show might hurt the ratings of the show. So I decided to test that hypothesis using the average monthly views as well as dislikes of the show with or without Joe.
+
+## Getting and storing data in database
+### Wrote a script to get the appropriate data of each episode from the Youtube channel of EverydayStruggle
+### For ones with Joe the file is called EverydayStruggleWithJoe/Get_data_with_Joe.py
+### For ones without Joe the file is called EverydayStruggleWithoutJoe/Get_data_without_Joe.py
+### From a python terminal, run each script. Eg python Get_data_without_Joe.py. This script will connect to the Everyday Youtube channel and download some relevant information of episodes that include number of views as well as number of dislikes for each episodes. Data for Joe or without Joe are stored in a sqlite database
+
+## Data Analysis
+### The python notebook called DataAbalysis.ipynb contains code that connects to both databases (with or without Joe), integrated both data sets, resampled data to monthly frequency and calculated the mean of view and dislikes for each month. Then plotted this information. 
+
+## Results
+### Monthly views of show with or without Joe
+
+### Monthly dislikes of show with or without Joe
+
+
+# First I need to get the data of the all the shows from 2017-04 to 2017-12. I wrote a python script to 
+
+# The youtube search api only returns a maximum of 50 results every request. So I decided to put this in a loop 
+# using a start date and a last date. After the first iteration, the last date is updated for the next iteration. However, I
+# didnt bother with the start date, so thats constant. However data is not duplicated since sqlite will not add data that is already
+# in database..so it will always find the last date and start from there. 
